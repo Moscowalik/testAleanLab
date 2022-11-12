bbeee997ecabc260b98849982102dd7e7fae4afe
Maxim Moskalik <90215347+Moscowalik@users.noreply.github.com>
2022-11-12 17:30:56 +0300

    delete consoleLog

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import JobBoard from "./components/JobBoard";
 import JobDetails from "./components/JobDetails";
 
 function App() {
-  const { pathname } = useLocation();
-  console.log(pathname);
   return (
     <div className="md:container md:mx-auto px-8">
       <section className="section py-20">
